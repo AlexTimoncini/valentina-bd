f407741741ae9c08d5fe503aac4bba654fbfc130
Alex <alex@overcover.com>
2024-04-08 17:47:55 +0200

    pwa new try

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'assets/icon48.png',
+   '/assets/icon72.png',
+   '/assets/icon96.png',
+   '/assets/icon144.png',
+   '/assets/icon192.png',
+   '/assets/icon512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Cache aperta');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+
+        // Altrimenti, effettua la richiesta al server
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['my-pwa-cache-v1'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
